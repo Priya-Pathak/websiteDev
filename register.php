a26d64c253b23c8f82a454f0027130d76dc956a7
Priya-Pathak <priyapathak24.07@gmail.com>
2021-01-22 16:18:28 +0530

    Cleaning code for register.php

--- register.php
+++ register.php
@@ -1,64 +1,68 @@
 <!DOCTYPE html>
-<html>
-    <head>
+<html lang="en-us">
+<head>
 
-    <?php
-        
-        if(isset($_POST['submit'])){
-            $userName = $_POST['uname'];
-            $email = $_POST['email'];
-            $pnumber = $_POST['pnumber'];
-            $msg = $_POST['msg'];
-            if($userName!='' && $email != '' && $pnumber != '' && $msg != ''){
 
-            }
-            else{
-                header("location:index.html");
-            }
+<?php
+    if(isset($_POST['submit'])){
+        $userName = $_POST['uname'];
+        $email = $_POST['email'];
+        $pnumber = $_POST['pnumber'];
+        $msg = $_POST['msg'];
+        if($userName!='' && $email != '' && $pnumber != '' && $msg != ''){        
+            //This is added so that browser does not go to landing page when form is left blank
+        }
+        else{
+            header("location:index.html");
+        }
             
         }
 
-        else{
+    else{
             header("location:index.html");
         } 
-        
-        ?>
-        
-        
-        <link rel="stylesheet" href="register.css">
-        <title>Landing Page</title>
-    </head>
-    <body>
+?>
 
-        <div class="header">
-            <hr>
-            <h1>XYZ</h1>
-            
-        </div>
-        <div class = "main">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="register.css">
+<title>Landing Page</title>
+</head>
+<body>
+
+
+<div class="header">
+<hr>
+<h1>XYZ</h1>
+</div>
+        
+        
+<div class="main">
        
-        <div class = "left">
-        <h1>Hi, <?php echo "<h4>".$_POST['uname']."</h4>";?></h1>
-        <h3>It was nice hearing from you</h3>
-        <h5>Thanks for your valuable feedback</h5>
 
-        <button onclick="location.href='index.html'" type="button">
-         Go back to home page</button>
+<div class="left">
+<h1>Hi, <?php echo "<h4>".$_POST['uname']."</h4>";?></h1>
+<h3>It was nice hearing from you</h3>
+<h5>Thanks for your valuable feedback</h5>
+<button onclick="location.href='index.html'" type="button">Go back to home page</button>
+</div>
         
-        </div>
-        <div class = "right">
-            <!-- <img src="https://cdn.pixabay.com/photo/2015/07/06/14/57/speech-833379__340.png"> -->
-            <img src="https://c.tenor.com/44LDvTiDM8cAAAAj/thanks-a-ton-thanks-a-bunch.gif">
-            <h3>Every note from you adds value to us</h3>
-            <h1>Thank You!!<h1>
-        </div>
+        
+<div class="right">
+<!-- <img src="https://cdn.pixabay.com/photo/2015/07/06/14/57/speech-833379__340.png"> -->
+<img src="https://c.tenor.com/44LDvTiDM8cAAAAj/thanks-a-ton-thanks-a-bunch.gif" alt="Thank you for your message!">
+<h3>Every note from you adds value to us</h3>
+<h1>Thank You!!<h1>
+</div>
         
 
-    </div>
-         <div id="footer">
-            <h5>© 2021 Datamatics Global Services Inc.</h5>
-        </div>
+</div>
 
-        
-    </body>
+
+<div id="footer">
+<h5>© 2021 XYZ Demo Services Inc.</h5>
+</div>
+
+
+</body>
 </html>
